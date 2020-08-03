33c4db95f8d8996bf1809b76bcc916bc04d89002
Krechals <topala.andrei@gmail.com>
2020-08-03 07:59:02 +0000

    fixed whitespacing

--- portfolio/src/main/webapp/oauth.js
+++ portfolio/src/main/webapp/oauth.js
@@ -1,11 +1,11 @@
 
 function signIn(googleUser) {
   const profile = googleUser.getBasicProfile();
-  const imagurl=profile.getImageUrl();
-  const name=profile.getName();
-  const email=profile.getEmail();
+  const imageUrl = profile.getImageUrl();
+  const name = profile.getName();
+  const email =profile.getEmail();
 
-  document.getElementById("myImg").src = imagurl;
+  document.getElementById("myImg").src = imageUrl;
   document.getElementById("name").innerHTML = name;
   document.getElementById("myP").style.visibility = "hidden";
 }
